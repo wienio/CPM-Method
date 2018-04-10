cac4d0217b6c28ffa3e4bb94c0c54afacff3c76f
wienio <wienio96@gmail.com>
2018-04-11 00:07:45 +0200

    work

--- main.js
+++ main.js
@@ -2,27 +2,43 @@
 // import * as jsPlumb from 'jsplumb.js'
 
 var TASKID = 0
-var taskForm = '<form>   </form>'
+var firstTaskForm = '<form><p>Nazwa zadania:</p><br><input type="text" class="task-input" required="true" placeholder="nazwa"><br>Czas trwania:<br><input type="text" value="0" readonly="readonly"><br><input type="submit" value="Zatwierdź">  </form>'
+var taskForm = '<form><label for="taskName">Nazwa zadania</label><input type="text" required="true" placeholder="nazwa">  </form>'
 
 class Task {
-    // Dodac trzeba cos zeby poprzednie taski byly powiazane
-    constructor(taskName, minTime, maxTime) {
+    constructor(taskName, durationTime, previousIds) {
         this.id = TASKID
         this.taskName = taskName
-        this.maxTime = minTime
-        this.maxTime = maxTime
+        this.durationTime = durationTime
+        this.previous = previousIds
     }
 }
 
 var taskList = []
 
 function addDiv() {
-    TASKID++;
     var taskDiv = document.createElement('div')
-    taskDiv.innerHTML = 'Dodaj informacje o zadaniu: ' + '<br><input type="text" name="myInputs[]">' + '<br><button>Zatwierdz</button>'
-    taskDiv.style.cssText = 'width: 30vh; height: 100px; background-color: red; margin: 5px;'
-    document.querySelector('.tasks').appendChild(taskDiv)
-    document.body.appendChild(taskDiv)
+    if (TASKID === 0) {
+        document.getElementById('file-input').disabled = true
+        taskDiv.innerHTML = firstTaskForm
+        taskDiv.className = 'task-container'
+    }
+    TASKID++;
+    
+    taskDiv.className = 'task-container'
+    taskDiv.id = 'task-container' + TASKID
+    // taskDiv.innerHTML = 'Dodaj informacje o zadaniu: ' + '<br><input type="text" name="myInputs[]">' + '<br><button>Zatwierdz</button>'
+    // taskDiv.style.cssText = 'width: 30vh; height: 100px; background-color: red; margin: 5px;'
+    document.getElementById('tasks').appendChild(taskDiv)
+
+    var els = document.querySelectorAll('.task-container')
+    jsPlumb.draggable(taskDiv)
+
+    jsPlumb.addEndpoint(taskDiv)
+    jsPlumb.connect({
+        source: taskDiv,
+        target: 'tasks'
+    })
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -33,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function checkVariablesIfIsntUndefined(objectList) {
     objectList.forEach((element) => {
-        if (typeof element.taskName === 'undefined' || typeof element.minTime === 'undefined' || typeof element.maxTime === 'undefined' || typeof element.id === 'undefined' ) {
+        if (typeof element.durationTime === 'undefined' || typeof element.taskName === 'undefined' || typeof element.id === 'undefined') {
             return false
         }
         return true;
@@ -73,13 +89,24 @@ function calculateCpm() {
 
 document.getElementById('file-input').addEventListener('change', readJsonTaskFile, false);
 
-jsPlumb.ready(function () {  
-    console.log("jsplumb dziala")
-    console.log(jsPlumb)
-    
-    // var e0 = jsPlumb.addEndpoint("container0",{uuid:"ep0"}) //set your own uuid for endpoint to access later.
-    // e1 = jsPlumb.addEndpoint("container1",{uuid:"ep1"});  
-    // jsPlumb.connect({ uuids:[e0.getUuid(),e1.getUuid()] }); 
-            // (or) 
-    // jsPlumb.connect({ uuids:["ep0","ep1"] });
+
+// jsPlumb.Defaults.MaxConnections = 5;
+
+
+jsPlumb.importDefaults({
+    Connector: ["Bezier", { curviness: 150 }],
+    Anchors: ["TopCenter", "BottomCenter"]
+});
+
+
+
+jsPlumb.bind("ready", function () {
+    jsPlumb.addEndpoint('tasks')
+    jsPlumb.addEndpoint('kontakt')
+
+    jsPlumb.connect({
+        source: 'tasks',
+        target: 'kontakt'
+    })
+    // your jsPlumb related init code goes here
 });
